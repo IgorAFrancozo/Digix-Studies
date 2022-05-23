e26724b870fe3eda84ebef1f1c41a4e3510e4a88
Bjorn <igorfrancozo9@gmai.com>
2022-05-23 04:10:56 -0300

    first

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './style.scss';
+
+
+class Botao extends React.Component {
+  render() {
+    return <button className="botao">Botao</button>;
+  }
+}
+
+export default Botao;
